437306e9ff3c0df5349f56a4fd026d3f7fe24549
Ashoke <ashokgorluashok143@gmail.com>
2023-05-03 04:30:51 +0000

    fetch

---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="contact-img"
+    />
+    <h1 className="contact-heading">Contact</h1>
+    <p className="contact-heading">phone no: 9515466657</p>
+  </div>
+)
+
+export default Contact
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://res.cloudinary.com/dxt3w8y0z/image/upload/v1682921498/MyPic_m2uin2.jpg"
+      alt="profile"
+    />
+    <h1 className="user-name">Ashoke Gorlu</h1>
+    <p className="user-designation">Software developer</p>
+  </div>
+)
+
+export default UserInfo
